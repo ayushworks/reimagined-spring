fe49874b3afc537132fdb3f32f391917643ce943
Ayush <ayushm4489@gmail.com>
2023-12-25 21:03:06 +0100

    enable component scan

--- src/main/java/com/ayush/demo/HelloResource.java
+++ src/main/java/com/ayush/demo/HelloResource.java
@@ -1,10 +1,12 @@
 package com.ayush.demo;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;;;
 
+@Component
 @Path("/hello")
 public class HelloResource {
     
