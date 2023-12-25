a1f0e293e60a2ee9dd0dcefd2fe7200164de8413
Ayush <ayushm4489@gmail.com>
2023-12-25 20:54:06 +0100

    using enviroment variables

--- src/main/java/com/ayush/demo/HelloResource.java
+++ src/main/java/com/ayush/demo/HelloResource.java
@@ -1,15 +1,19 @@
 package com.ayush.demo;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;;;
 
 @Path("/hello")
 public class HelloResource {
     
+    @Value("${name}")
+    String name;
 
     @GET
     public String sayHello(){
-        return "Hello From Spring"; 
+        return "Hello "+name +", From Spring"; 
     }
 
 }
